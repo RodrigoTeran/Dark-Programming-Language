bc89a9ea652959061170edf3361cb1fb0dc21398
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-03-09 10:45:00 -0600

    Now multiple reruns can be made

--- src/dark/parse.js
+++ src/dark/parse.js
@@ -1,12 +1,10 @@
 import nearley from "nearley";
 import grammar from "./parsed-dark";
 
-// Create a Parser object from our grammar.
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-
 export const codeToParse = (code) => {
+  // Create a Parser object from our grammar.
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
   parser.feed(code);
   // console.log(JSON.stringify(parser.results[0], null, "    "));
-
   return parser.results[0];
 };
